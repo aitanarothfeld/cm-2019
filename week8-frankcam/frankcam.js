731d5bd0ef722b909503bd1dab612aa761e4c6cc
Aitana Rothfeld <aitanarothfeld@gmail.com>
2019-10-15 21:02:59 -0700

    add frankcam.js

---
+++ week8-frankcam/frankcam.js
@@ -0,0 +1,10 @@
+// npm install livecam
+ 
+const LiveCam = require('livecam');
+const webcam_server = new LiveCam({
+    'start' : function() {
+        console.log('WebCam server started!');
+    }
+});
+ 
+webcam_server.broadcast();
